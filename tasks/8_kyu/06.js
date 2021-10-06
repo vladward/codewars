2df46ddac4d5a9dc46230d8aaf205857f4b83ab5
vladward <vladward13@gmail.com>
2021-10-06 17:29:41 +0300

    feature: Define a card suit

---
+++ tasks/8_kyu/06.js
@@ -0,0 +1,15 @@
+/*
+Define a card suit
+ */
+
+function defineSuit(card) {
+    if (card.includes('♣')) {
+        return 'clubs'
+    } else if (card.includes('♦')) {
+        return 'diamonds'
+    } else if (card.includes('♥')) {
+        return 'hearts'
+    } else if (card.includes('♠')) {
+        return 'spades'
+    }
+}
