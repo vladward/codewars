fd79d60d8ce2e69850a1ab71f39fd24aaa7ac133
vladward <vladward13@gmail.com>
2021-10-06 17:16:16 +0300

    feature: Safen User Input Part I - htmlspecialchars

---
+++ tasks/8_kyu/02.js
@@ -0,0 +1,10 @@
+/*
+Safen User Input Part I - htmlspecialchars
+ */
+
+function htmlspecialchars(formData) {
+    return formData.replace(/&/g, "&amp;")
+        .replace(/"/g, "&quot;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;");
+}
