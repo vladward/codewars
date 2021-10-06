771e9dd792640d8207793e27f2fda608b2a6c1cf
vladward <vladward13@gmail.com>
2021-10-06 18:52:41 +0300

    feature: Vowel Count

--- tasks/7_kyu/02.js
+++ tasks/7_kyu/02.js
@@ -1,14 +1,7 @@
 /*
-254 shades of grey
+Vowel Count
  */
 
-function shadesOfGrey(n) {
-    var shades = [];
-
-    for (var i = 1; i <= Math.min(n, 254); i++) {
-        var grey = ("0" + i.toString(16)).slice(-2);
-        shades.push("#" + grey + grey + grey);
-    }
-
-    return shades;
+function getCount(str) {
+    return (str.match(/[aeiou]/gi) || []).length;
 }
