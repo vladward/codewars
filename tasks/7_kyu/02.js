1bd696e5a2213b2e6190269eecf4a6818840ffb1
vladward <vladward13@gmail.com>
2021-10-06 18:48:24 +0300

    feature: 254 shades of grey

---
+++ tasks/7_kyu/02.js
@@ -0,0 +1,14 @@
+/*
+254 shades of grey
+ */
+
+function shadesOfGrey(n) {
+    var shades = [];
+
+    for (var i = 1; i <= Math.min(n, 254); i++) {
+        var grey = ("0" + i.toString(16)).slice(-2);
+        shades.push("#" + grey + grey + grey);
+    }
+
+    return shades;
+}
