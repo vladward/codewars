a1a44773b7183cdfe78932a637205eadfd31fb56
vladward <vladward13@gmail.com>
2021-10-06 18:31:04 +0300

    feature: Find Multiples of a Number

---
+++ tasks/8_kyu/27.js
@@ -0,0 +1,13 @@
+/*
+Find Multiples of a Number
+ */
+
+function findMultiples(integer, limit) {
+    const result = []
+
+    for (let i = integer; i <= limit; i += integer) {
+        result.push(i)
+    }
+
+    return result
+}
