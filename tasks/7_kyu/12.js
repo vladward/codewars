4007dddc559ade793369ceaf51679da0a3434ac1
vladward <vladward13@gmail.com>
2021-10-06 19:13:04 +0300

    feature: Beginner Series #3 Sum of Numbers

---
+++ tasks/7_kyu/12.js
@@ -0,0 +1,9 @@
+/*
+Beginner Series #3 Sum of Numbers
+ */
+
+const GetSum = (a, b) => {
+    let min = Math.min(a, b),
+        max = Math.max(a, b);
+    return ((max - min + 1) * (min + max)) / 2;
+};
