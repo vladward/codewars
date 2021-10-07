876b6350b2c39b51cc036a0fdac9100f1a644afa
vladward <vladward13@gmail.com>
2021-10-07 11:18:06 +0300

    feature: Find the first non-consecutive number

---
+++ tasks/8_kyu/67.js
@@ -0,0 +1,10 @@
+/*
+Find the first non-consecutive number
+ */
+
+function firstNonConsecutive(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i - 1] + 1 !== arr[i]) return arr[i];
+    }
+    return null;
+}
