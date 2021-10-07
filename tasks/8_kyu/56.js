7fdcda02ba44309ae63323c1e7212384cfd1dc9c
vladward <vladward13@gmail.com>
2021-10-07 10:43:29 +0300

    feature: Sum without highest and lowest number

---
+++ tasks/8_kyu/56.js
@@ -0,0 +1,16 @@
+/*
+Sum without highest and lowest number
+ */
+
+const sumArray = array => {
+    if (!array) {
+        return 0
+    }
+    let s = 0;
+    array.sort((a, b) => a - b).forEach((a, i) => {
+        if (i !== array.length - 1 && i !== 0) {
+            s += a;
+        }
+    })
+    return s;
+}
