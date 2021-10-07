21b8cc36660b87cdf5773dc1118d384bfe7a0ed3
vladward <vladward13@gmail.com>
2021-10-07 11:37:22 +0300

    feature: altERnaTIng cAsE <=> ALTerNAtiNG CaSe

---
+++ tasks/8_kyu/81.js
@@ -0,0 +1,10 @@
+/*
+altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+ */
+
+String.prototype.toAlternatingCase = function() {
+    return this.replace(
+        /[a-z]/gi,
+        char => (/[a-z]/.test(char) ? char.toUpperCase() : char.toLowerCase())
+    )
+}
