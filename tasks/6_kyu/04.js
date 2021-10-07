937b3c8a879c4a3ad7634644c177e885633b1d47
vladward <vladward13@gmail.com>
2021-10-07 10:18:04 +0300

    feature: Sum of Digits / Digital Root

---
+++ tasks/6_kyu/04.js
@@ -0,0 +1,12 @@
+/*
+Sum of Digits / Digital Root
+ */
+
+
+function digital_root(n) {
+    if (n < 10) return n;
+    return digital_root(
+        n.toString().split('').reduce((acc, d) => {
+            return acc + +d;
+        },0));
+}
